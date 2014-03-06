b58841f5b2a2d879d5d6aed93b4bf804045b4b65
werdbrian <werdbrian@gmail.com>
2014-03-05 18:00:17 -0800

    three test cases testing sym-link stuff

--- block-del-sym-link-tests.pl
+++ block-del-sym-link-tests.pl
@@ -1,4 +1,4 @@
-02#! /usr/bin/perl -w
+#! /usr/bin/perl -w
 
 open(FOO, "ospfsmod.c") || die "Did you delete ospfsmod.c?";
 $lines = 0;
@@ -6,89 +6,17 @@ $lines++ while defined($_ = <FOO>);
 close FOO;
 
 @tests = (
-    # test reading
-    [ 'diff base/hello.txt test/hello.txt >/dev/null 2>&1 && echo $?',
-      "0"
+    # create a symbolic link
+    [ 'ln -s test/hello.txt test/hello2.txt; ls base | grep hello2',
+      "hello2.txt"
     ],
-    
-    [ 'cmp base/pokercats.gif test/pokercats.gif >/dev/null 2>&1 && echo $?',
-      "0"
+    #delete a symbolic link
+    [ 'unlink test/hello2.txt; ls base | grep hello2',
+      ""
     ],
-        
-    [ 'ls -l test/pokercats.gif | awk "{ print \$5 }"',
-      "91308"
-    ],
-
-    # test writing
-    # We use dd to write because it doesn't initially truncate, and it can
-    # be told to seek forward to a particular point in the disk.
-    [ "echo Bybye | dd bs=1 count=5 of=test/hello.txt conv=notrunc >/dev/null 2>&1 ; cat test/hello.txt",
-      "Bybye, world!"
-    ],
-    
-    [ "echo Hello | dd bs=1 count=5 of=test/hello.txt conv=notrunc >/dev/null 2>&1 ; cat test/hello.txt",
-      "Hello, world!"
-    ],
-    
-    [ "echo gi | dd bs=1 count=2 seek=7 of=test/hello.txt conv=notrunc >/dev/null 2>&1 ; cat test/hello.txt",
-      "Hello, girld!"
-    ],
-    
-    [ "echo worlds galore | dd bs=1 count=13 seek=7 of=test/hello.txt conv=notrunc >/dev/null 2>&1 ; cat test/hello.txt",
-      "Hello, worlds galore"
-    ],
-    
-    [ "echo 'Hello, world!' > test/hello.txt ; cat test/hello.txt",
-      "Hello, world!"
-    ],
-    
-    # create a file
-    [ 'touch test/file1 && echo $?',
-      "0"
-    ],
-
-    # read directory
-    [ 'touch test/dir-contents.txt ; ls test | tee test/dir-contents.txt | grep file1',
-      'file1'
-    ],
-
-    # write files, remove them, then read dir again
-    [ 'ls test | dd bs=1 of=test/dir-contents.txt >/dev/null 2>&1; ' .
-      ' touch test/foo test/bar test/baz && '.
-      ' rm    test/foo test/bar test/baz && '.
-      'diff <( ls test ) test/dir-contents.txt',
-      ''
-    ],
-
-    # remove the last file
-    [ 'rm -f test/dir-contents.txt && ls test | grep dir-contents.txt',
-      ''
-    ],
-
-
-    # write to a file
-    [ 'echo hello > test/file1 && cat test/file1',
-      'hello'
-    ],
-    
-    # append to a file
-    [ 'echo hello > test/file1 ; echo goodbye >> test/file1 && cat test/file1',
-      'hello goodbye'
-    ],
-
-    # delete a file
-    [ 'rm -f test/file1 && ls test | grep file1',
-      ''
-    ],
-
-    # make a larger file for indirect blocks
-    [ 'yes | head -n 5632 > test/yes.txt && ls -l test/yes.txt | awk \'{ print $5 }\'',
-      '11264'
-    ],
-   
-    # truncate the large file
-    [ 'echo truncernated11 > test/yes.txt | ls -l test/yes.txt | awk \'{ print $5 }\' ; rm test/yes.txt',
-      '15'
+    #delete the symbolic link created for us originally when copied from base folder
+    [ 'unlink test/link; ls base | grep link',
+      ""
     ],
 
 );
